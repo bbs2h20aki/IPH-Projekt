0dbc416ed497d64ecffa0ddc4a11b4a371bb065b
Johannes Stümer / BBS2H20A <Johannes.Stuemer@edu.bib.de>
2021-09-23 10:31:08 +0200

    added pickerview method

--- Crypto-Master/Crypto-Master/ViewController.swift
+++ Crypto-Master/Crypto-Master/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let countries = ["Ceasar", "Vegenere", "AES"]
+    
+    let pickerView = UIPickerView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        textfield.inputView = pickerView
     }
 
-
+ 
+    @IBOutlet weak var textfield: UITextField!
+    
+    
+}
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textfield.text = countries[row]
+        textfield.resignFirstResponder()
+    }
 }
 
