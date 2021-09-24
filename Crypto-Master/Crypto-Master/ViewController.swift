e20ad51ab2da6ae256feb6d6a961d0023ec9d500
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2021-09-24 09:29:59 +0200

    aes integration

--- Crypto-Master/Crypto-Master/ViewController.swift
+++ Crypto-Master/Crypto-Master/ViewController.swift
@@ -6,6 +6,14 @@
 //
 
 import UIKit
+import Foundation
+import CommonCrypto
+
+
+
+
+
+
 
 class ViewController: UIViewController {
 
@@ -46,6 +54,7 @@ class ViewController: UIViewController {
         let vigenere2 = Vigenere( key: key.text!)
         let ceasar2 = cesar()
         
+        
         let choice = textfield.text!
         let message = eingabe.text!
         
@@ -60,9 +69,9 @@ class ViewController: UIViewController {
                 ausgabe.text = vigenere2.encrypt(plainText: message)
                 break
                 
-           // case "AES":
-               
-               // break
+            case "AES":
+                ausgabe.text = encrypted?.base64EncodedString()
+                break
           
             default:
                 ausgabe.text = ""
@@ -79,7 +88,9 @@ class ViewController: UIViewController {
                 ausgabe.text = vigenere2.decrypt(encryptedText: message)
                 break
                 
-           // case "AES":
+            case "AES":
+                ausgabe.text = decrypted?.base64EncodedString()
+                break
             
             case "": break
             
@@ -108,5 +119,133 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
         textfield.text = countries[row]
         textfield.resignFirstResponder()
     }
+    
+    
+    
+    
+
+
 }
+
+class AES{
+func crypt(operation: Int, algorithm: Int, options: Int, key: Data,
+
+        initializationVector: Data, dataIn: Data) -> Data? {
+
+    return key.withUnsafeBytes { keyUnsafeRawBufferPointer in
+
+        return dataIn.withUnsafeBytes { dataInUnsafeRawBufferPointer in
+
+            return initializationVector.withUnsafeBytes { ivUnsafeRawBufferPointer in
+
+
+                let dataOutSize: Int = dataIn.count + kCCBlockSizeAES128*2
+
+                let dataOut = UnsafeMutableRawPointer.allocate(byteCount: dataOutSize,
+
+                    alignment: 1)
+
+                defer { dataOut.deallocate() }
+
+                var dataOutMoved: Int = 0
+
+                let status = CCCrypt(CCOperation(operation), CCAlgorithm(algorithm),
+
+                    CCOptions(options),
+
+                    keyUnsafeRawBufferPointer.baseAddress, key.count,
+
+                    ivUnsafeRawBufferPointer.baseAddress,
+
+                    dataInUnsafeRawBufferPointer.baseAddress, dataIn.count,
+
+                    dataOut, dataOutSize, &dataOutMoved)
+
+                guard status == kCCSuccess else { return nil }
+
+                return Data(bytes: dataOut, count: dataOutMoved)
+
+            }
+
+        }
+
+    }
+
+}
+
+ 
+ 
+
+func randomGenerateBytes(count: Int) -> Data? {
+
+    let bytes = UnsafeMutableRawPointer.allocate(byteCount: count, alignment: 1)
+
+    defer { bytes.deallocate() }
+
+    let status = CCRandomGenerateBytes(bytes, count)
+
+    guard status == kCCSuccess else { return nil }
+
+    return Data(bytes: bytes, count: count)
+
+}
+    
+    
+}
+
+extension Data {
+   
+
+    func encryptAES256_CBC_PKCS7_IV(key: Data) -> Data? {
+
+        guard let iv = aes.randomGenerateBytes(count: kCCBlockSizeAES128) else { return nil }
+        
+        guard let ciphertext = aes.crypt(operation: kCCEncrypt,
+
+                                    algorithm: kCCAlgorithmAES,
+
+                                    options: kCCOptionPKCS7Padding,
+
+                                    key: key,
+
+                                    initializationVector: iv,
+
+                                    dataIn: self) else { return nil }
+
+         
+
+        return iv + ciphertext
+
+    }
+
+
+
+    func decryptAES256_CBC_PKCS7_IV(key: Data) -> Data? {
+
+        guard count > kCCBlockSizeAES128 else { return nil }
+
+        let iv = prefix(kCCBlockSizeAES128)
+
+        let ciphertext = suffix(from: kCCBlockSizeAES128)
+
+        return aes.crypt(operation: kCCDecrypt, algorithm: kCCAlgorithmAES,
+
+            options: kCCOptionPKCS7Padding, key: key, initializationVector: iv,
+
+            dataIn: ciphertext)
+
+    }
+
+}
+
+let aes = AES()
+let key = aes.randomGenerateBytes(count: 256/8)!
+let vc = ViewController()
+
+let ein = vc.eingabe.text
+let superDuperSecret =  ein?.data(using: .utf8)!
+
+let encrypted = superDuperSecret?.encryptAES256_CBC_PKCS7_IV(key: key)!
+
+let decrypted = encrypted?.decryptAES256_CBC_PKCS7_IV(key: key)!
 
