184dd956457f6401a6cf1f87a26515136a1f7280
Johannes Stümer / BBS2H20A <Johannes.Stuemer@edu.bib.de>
2021-09-24 08:38:00 +0200

    erklaerung updated

--- Crypto-Master/Crypto-Master/ViewController.swift
+++ Crypto-Master/Crypto-Master/ViewController.swift
@@ -54,12 +54,10 @@ class ViewController: UIViewController {
         if e {
             switch choice {
             case "Ceasar":
-                erklaerung.text = "Bei der Verschlüsselung wird jeder Buchstabe des Klartexts in Geheimtextbuchstaben abgebildet. Diese Abbildung ergibt sich, indem man die Zeichen eines geordneten Alphabets um eine bestimmte Anzahl zyklisch nach rechts verschiebt; zyklisch bedeutet, dass man beim Verschieben über Z hinaus wieder bei A anfangend weiterzählt."
                 let s = Int(key.text!)!
                 ausgabe.text = ceasar2.encrypt(message: message , shift: s)
                 break
             case "Vigenere":
-                erklaerung.text = "Der Klartext wird in Einzelzeichen zerlegt und diese durch Geheimtextzeichen ersetzt, die mithilfe eines Kennworts aus mehreren unterschiedlichen Alphabeten des Vigenere-Quadrats ausgewählt werden. Dabei handelt es sich um eine quadratische Anordnung von untereinander stehenden verschobenen Alphabeten."
                 ausgabe.text = vigenere2.encrypt(plainText: message)
                 break
                 
@@ -110,6 +108,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         textfield.text = countries[row]
         textfield.resignFirstResponder()
+        if textfield.text == "Ceasar"{
+        erklaerung.text = "Bei der Verschlüsselung wird jeder Buchstabe des Klartexts in Geheimtextbuchstaben abgebildet. Diese Abbildung ergibt sich, indem man die Zeichen eines geordneten Alphabets um eine bestimmte Anzahl zyklisch nach rechts verschiebt; zyklisch bedeutet, dass man beim Verschieben über Z hinaus wieder bei A anfangend weiterzählt."
+        }
+        if textfield.text == "Vigenere"{
+        erklaerung.text = "Der Klartext wird in Einzelzeichen zerlegt und diese durch Geheimtextzeichen ersetzt, die mithilfe eines Kennworts aus mehreren unterschiedlichen Alphabeten des Vigenere-Quadrats ausgewählt werden. Dabei handelt es sich um eine quadratische Anordnung von untereinander stehenden verschobenen Alphabeten."
+        }
     }
 }
 
